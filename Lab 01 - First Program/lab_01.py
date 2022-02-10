283113b02245fc16da4feb7ddcb7dade6dc71e21
PRuizVidaurre <p.ruizv@edu.uah.es>
2022-02-10 18:47:01 +0100

    Prueba 0.2 The best story ever told(I cried)

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,4 @@
-print("Tengo sueño y quiero irme a casa :v ")
-
-
+print("An old man approached me in the middle of the park and said:\"What are you doing here alone my child?\"")
+print("I was petrified by him,not by what he said, but what he looked like he was \n¡TERRYFING|")
+print("as calm as i colud be I answered him:\"My mom told me not to talk to strangers.\"")
+print("He looked me dead in the eyes\nI ran,\npooped my pants \nand went home")
